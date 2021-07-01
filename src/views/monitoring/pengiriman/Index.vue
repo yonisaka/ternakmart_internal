b394fe8c29b1c6ea6692f2d875ef75015f4f9b72
yonisaka <yonisaka0@gmail.com>
2021-07-01 18:48:54 +0700

    add menu pengiriman

--- src/views/monitoring/pengiriman/Index.vue
+++ src/views/monitoring/pengiriman/Index.vue
@@ -62,13 +62,44 @@
                         <template v-slot:[`item.total_harga`]="{ item }">
                             <span>{{ formatPrice(item.total_harga) }}</span>
                         </template>
-                        <template v-slot:[`item.actions`]="{ item }">
+                         <template v-slot:[`item.transaksi_st`]="{ item }">
+                            <v-chip v-if="item.transaksi_st == 'PAID'" 
+                            color="success"
+                            text-color="white"
+                            small
+                            >
+                                PAID
+                            </v-chip>
+                            <v-chip v-else-if="item.transaksi_st == 'PENDING'" 
+                            color="orange"
+                            text-color="white"
+                            small
+                            >
+                                PENDING
+                            </v-chip>
+                            <v-chip v-else-if="item.transaksi_st == 'EXPIRED'" 
+                            color="red"
+                            text-color="white"
+                            small
+                            >
+                                EXPIRED
+                            </v-chip>
+                            <v-chip v-else-if="item.transaksi_st == 'CART'" 
+                            color="orange"
+                            text-color="white"
+                            small
+                            >
+                                Cart
+                            </v-chip>
+                        </template>
+                        <template v-slot:[`item.pengiriman_st`]="{ item }">
+                            <span>{{ item.pengiriman_st }}</span>
                             <v-btn 
-                            small 
+                            x-small 
                             icon
                             color="primary" 
-                            class="mr-2" 
-                            @click="showForm(item.id)"
+                            class="mx-2" 
+                            @click="showDialog(item.id)"
                             >
                                 <v-icon>
                                 mdi-pencil
@@ -88,6 +119,26 @@
                 </v-row>
             </v-card-text>
         </v-card>
+        <v-dialog v-model="dialog" max-width="400px">
+            <v-card>
+                <v-card-title class="headline">
+                    <span class="mx-auto"> Status Pengiriman </span>
+                </v-card-title>
+                <v-card-text>
+                    <v-select
+                    v-model="detail.pengiriman_st"
+                    :items="items"
+                    label="Status Pengiriman"
+                    ></v-select>
+                </v-card-text>
+                <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDialog">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="dialogItemConfirm(detail.id)">Update</v-btn>
+                <v-spacer></v-spacer>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-container>
 </template>
 <script>
@@ -120,16 +171,19 @@ export default {
                 // { text: "Harga Ternak", align: "center", sortable: false, value: "ternak_harga" },
                 { text: "Harga Ternak", align: "center", sortable: false, value: "total_harga" },
                 { text: "Status", align: "center", sortable: false, value: "transaksi_st" },
-                { text: "Tanggal", align: "center", sortable: false, value: "transaksi_tanggal" },
-                { text: "Actions", align: "center", sortable: false, value: "actions" },
+                // { text: "Tanggal", align: "center", sortable: false, value: "transaksi_tanggal" },
+                // { text: "Actions", align: "center", sortable: false, value: "actions" },
+                { text: "Status Pengiriman", align: "center", sortable: false, value: "pengiriman_st" },
             ],
-            pengiriman:[]
+            pengiriman:[],
+            items: ['Sedang Disiapkan', 'Dalam Perjalanan', 'Sudah Diterima'],
+            dialog: false,
+            detail: {}
         }
     },
     mounted() {
         axios.get("/transaksi/pengiriman")
             .then((res) => {
-                console.log(res.data.pengiriman)
                 this.pengiriman = res.data.pengiriman
             })
     },
@@ -137,6 +191,34 @@ export default {
         formatPrice(value) {
             let val = (value/1).toFixed(2).replace('.', ',')
             return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+        },
+        closeDialog () {
+            this.dialog = false
+        },
+        showDialog(id) {
+            console.log(id)
+            axios
+            .get("/transaksi/"+ id +"/detail")
+            .then((res) => {
+                console.log(res.data.cart)
+                this.detail = res.data.cart
+                this.dialog = true
+            })
+        },
+        dialogItemConfirm (id) {
+            // console.log(id)
+            // console.log(this.detail)
+            axios
+            .put("/transaksi/"+ id, this.detail)
+            .then((res) => {
+                console.log(res)
+                this.snackbar = true
+                this.message = 'Berhasil Update Data'
+                this.color = '#139CA4'
+                this.dialogStatus = false
+                setTimeout( () => this.$router.go(), 1000);
+            })
+            .catch((err) => console.log(err));
         },
     }
 }
